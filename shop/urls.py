07dfc9a97738dad70b099c322fa5b2c8ecc26993
prmpn <prmpnppgrp@gmail.com>
2024-03-05 14:51:59 +0200

    slug convertor for categories

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CatalogView, ProductView
+from .views import CatalogView, ProductDetailView
 
 urlpatterns = [
-    path('catalog/', CatalogView.as_view(), name='catalog'),
-    path('catalog/product', ProductView.as_view(), name='product'),
+    path('catalog/<slug:category_slug>', CatalogView.as_view(), name='catalog'),
+    # path('catalog/product', ProductView.as_view(), name='product'),
+    path('catalog/product/<slug:product_slug>', ProductDetailView.as_view(), name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView, ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
 from shop.models import Product
 
@@ -9,10 +10,27 @@ class CatalogView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.kwargs['category_slug'] == 'all':
+            context['products'] = Product.objects.all()
+        else:
+            context['products'] = get_object_or_404(Product.objects.filter(category__slug=self.kwargs['category_slug']))
+
         context['title'] = 'Каталог'
-        context['products'] = Product.objects.all()
         return context
 
 
-class ProductView(TemplateView):
+# class ProductView(TemplateView):
+#     template_name = 'shop/product.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
     template_name = 'shop/product.html'
+    slug_field = 'product_slug'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = Product.objects.get(slug=self.kwargs['slug'])
+        return context
+
